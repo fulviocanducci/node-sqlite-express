818970407d0608f07d20fbf027d6800626bd13ab
Fulvio <fulviocanducci@hotmail.com>
2019-08-26 13:20:31 -0300

    todos-router.js

--- routers/todos-routers.js
+++ routers/todos-routers.js
@@ -35,7 +35,7 @@ module.exports = {
                 done
             }, {
                 where: { 
-                    id: id 
+                    id
                 }
             })
             .then((result) => { res.json(result) })
