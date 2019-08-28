d397801aad4ae2e68ac7d988377957df3c95dd30
Fulvio <fulviocanducci@hotmail.com>
2019-08-28 16:33:04 -0300

    change port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const authRouter = require('./routers/auths-routers');
 * Express Server Http
 */
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 /*
 * Routers Configuration
