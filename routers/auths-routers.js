48d0ae4561a26d685b90154e8cb025e5e2afa6ab
Fulvio <fulviocanducci@hotmail.com>
2019-08-29 11:53:35 -0300

    email or password undefine

--- routers/auths-routers.js
+++ routers/auths-routers.js
@@ -8,43 +8,55 @@ module.exports = {
         router.post('/login', (req, res) => {            
             const email = req.body.email;
             const password = req.body.password;
-            userModels.findOne({
-                attributes: ['id', 'name', 'email', 'password'],
-                where: {
-                    email
-                }
-            })
-            .then((result) => {                 
-                if (bcrypt.compareSync(password, result.password, salt.value)) {                                          
-                    const data = {
-                        user: {
-                            id: result.id,
-                            name: result.name,
-                            email: result.email
-                        },
-                        audience: 'urn:foo',
-                        issuer: 'urn:issuer',
-                        sub: result.id, 
-                        jwtid: result.id,
-                        name: result.email,
-                    };
-                    var token = auth.createToken(data);
-                    res.json({
-                        token, 
-                        'status': 200,
-                        'description': 'Login valid'
-                    });
-                } else {
-                    res
-                        .status(404)
-                        .json({
-                            'token':'',
-                            'status': 404,
-                            'description': 'Login invalid'
+            if (typeof email === 'undefined' || typeof password === 'undefined') {
+                res.status(404).json({
+                    token: '', 
+                    status: 404,
+                    description: 'Error: Empty email or password'
+                });
+            
+            } else {
+                userModels.findOne({
+                    attributes: ['id', 'name', 'email', 'password'],
+                    where: {
+                        email
+                    }
+                })
+                .then((result) => {                 
+                    if (bcrypt.compareSync(password, result.password, salt.value)) {                                          
+                        const data = {
+                            user: {
+                                id: result.id,
+                                name: result.name,
+                                email: result.email
+                            },
+                            dates: {
+                                createdAt: new Date()
+                            },
+                            audience: 'urn:foo',
+                            issuer: 'urn:issuer',
+                            sub: result.id, 
+                            jwtid: result.id,
+                            name: result.email,
+                        };
+                        var token = auth.createToken(data);
+                        res.json({
+                            token, 
+                            'status': 200,
+                            'description': 'Login valid'
                         });
-                }                
-            })
-            .catch((error) => { res.json(error) });
+                    } else {
+                        res
+                            .status(404)
+                            .json({
+                                'token':'',
+                                'status': 404,
+                                'description': 'Login invalid'
+                            });
+                    }                
+                })
+                .catch((error) => { res.json(error) });
+            }
         });
     }
 }
